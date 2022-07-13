55ecdad157b98fda9f8988803ee992d5f76dc662
chase <chase@pop-os.localdomain>
2022-07-13 16:20:58 -0400

    initial

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Link from "./lib/Link.svelte";
+</script>
+
+<main>
+  <Link />
+</main>
+
+<style>
+ 
+</style>
---
+++ src/lib/Link.svelte
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<section>
+    <div>
+        <h2>refurbished<br />laptops</h2>
+        <p>Power your workday with a range of reconditioned laptops.</p>
+        <h3>shop now</h3>
+    </div>
+
+    <img
+        src="/laptop-fixed.png"
+        alt="a laptop computer"
+    />
+</section>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    section {
+        display: flex;
+        box-shadow: 0px 1px 9px -1px rgba(0, 0, 0, 0.25);
+        place-items: center;
+    }
+    div {
+        padding: 20px
+    }
+    h2{
+        text-transform: uppercase;
+        font-size: 16px;
+        font-weight: 300;
+        margin-bottom: 12px;
+    }
+    p{
+        margin-bottom: 16px;
+    }
+</style>
